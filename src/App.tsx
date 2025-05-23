8e77697b80d1e2a5188ce4ffcda3eb701f708392
BSIT606J2022 <116421244+BSIT606J2022@users.noreply.github.com>
2025-05-23 10:06:44 +0300

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -7,23 +7,20 @@ import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import PostJob from "./pages/PostJob";
 import Navbar from "./components/Navbar";
-import { AuthProvider } from "./context/AuthContext";
 
 const App: React.FC = () => {
   return (
-    <AuthProvider>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/jobs" element={<JobList />} />
-          <Route path="/jobs/:id" element={<JobDetails />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/post-job" element={<PostJob />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/jobs" element={<JobList />} />
+        <Route path="/jobs/:id" element={<JobDetails />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/post-job" element={<PostJob />} />
+      </Routes>
+    </Router>
   );
 };
 
